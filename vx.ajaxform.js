0b23899dd7bf2d2aaa6291b89d12f4acf6637d9c
antimatter15 <antimatter15@0e141230-aabc-11dd-8af2-493fb0bb9f7c>
2008-11-04 22:09:45 +0000

    fixed, renamed

--- vx.ajaxform.js
+++ vx.ajaxform.js
@@ -1,3 +1,3 @@
 /* vX AJAX Post Form JS plugin by soren121 */
 /*Requires Ajax Submit*/
-_.ajaxForm=function(f,r){_.E(_.G(f),"submit",function(e){e.preventDefault();ajaxSubmit(f,r)})}
+_.ajaxform=function(f,r){_.E(_.G(f),"submit",function(e){e.preventDefault();_.ajaxsubmit(f,r)})}
