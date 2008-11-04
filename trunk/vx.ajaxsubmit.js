19fbcfa4dc19f59e30e1e0bee504b01a492c8f10
antimatter15 <antimatter15@0e141230-aabc-11dd-8af2-493fb0bb9f7c>
2008-11-04 22:09:45 +0000

    fixed, renamed

--- trunk/vx.ajaxsubmit.js
+++ trunk/vx.ajaxsubmit.js
@@ -1,2 +1,2 @@
 /* vX AJAX Post Form JS plugin by soren121 */
-_.ajaxSubmit=function(c,r){var t=_.G(c),z={},y=t.getElementsByTagName("*");for(var i=y.length;i--;) z[y[i].name]=y[i].value;_.X(t.action,r,'?'+_.Q(z),t.method=="post")}
+_.ajaxsubmit=function(c,r){var t=_.G(c),z={},y=t.getElementsByTagName("*");for(var i=y.length;i--;) z[y[i].name]=y[i].value;_.X(t.action,r,'?'+_.Q(z),t.method=="post")}
