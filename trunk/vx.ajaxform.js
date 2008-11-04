e93cb0b752c81cf191cb62ca6ed7daa97366e620
antimatter15 <antimatter15@0e141230-aabc-11dd-8af2-493fb0bb9f7c>
2008-11-04 22:06:58 +0000

    added the ajax form stuff

---
+++ trunk/vx.ajaxform.js
@@ -0,0 +1,2 @@
+/* vX AJAX Post Form JS plugin by soren121 */
+ajaxForm=function(f,r){_.E(_.G(f),"submit",function(e){e.preventDefault();ajaxSubmit(f,r)})}
---
+++ trunk/vx.ajaxsubmit.js
@@ -0,0 +1,2 @@
+/* vX AJAX Post Form JS plugin by soren121 */
+ajaxSubmit=function(c,r){var t=_.G(c),z={},y=t.getElementsByTagName("*");for(var i=y.length;i--;) z[y[i].name]=y[i].value;_.X(t.action,r,'?'+_.Q(z),t.method=="post")}
