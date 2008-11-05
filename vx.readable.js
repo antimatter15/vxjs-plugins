6026b774ed8b0d25b5b446c6a3728824b151dab6
antimatter15 <antimatter15@0e141230-aabc-11dd-8af2-493fb0bb9f7c>
2008-11-05 01:54:38 +0000

    vx readable translator

---
+++ vx.readable.js
@@ -0,0 +1,3 @@
+(function(){
+for(var x in _){_["ajax,anim,class,clone,event,extend,fade,id,htmlentities,index,namespace,queryencode,queue,ready,json,template".split(",")["XALCETFGHINQURSM".indexOf(x)]]=_[x]}
+})()
