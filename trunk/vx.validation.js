1e1d868dcbe3d4010f67aecad6df6918ffb2d1e1
antimatter15 <antimatter15@0e141230-aabc-11dd-8af2-493fb0bb9f7c>
2008-11-06 04:53:08 +0000

    updated validation

--- trunk/vx.validation.js
+++ trunk/vx.validation.js
@@ -5,7 +5,7 @@ _._validation = {
   required: ["keyup",function(el){
     return !!el.value
   }],
-  email: ["keyup",function(element){
+  email: ["keyup",function(el){
     
   }],
   number: ["keyup",function(el){
@@ -17,28 +17,33 @@ _._validation = {
 }
 
 _.validate = function(h){
-  var l, c, v, i
+  var l, c, v, i, k
+  
   for(l in _._validation){
     c=_.L(l,h) //get all of class
     v=_._validation[l] //the thingy that does validation
-    for(i=c.length;i--;){ //loop elements
-      _.E(c[i], "keyup", function(e){ //attach event (defined by config)
-        console.log("sumptin happenz")
-        e.target._v=e.target._v?e.target._v:[]; //if no validtor, then create
-        if(!v[1](e.target)){ //test if fail
-          console.log("fail",e.target)
-          e.target._v.push(l); //phail! add to fail-list
-          _.addclass(e.target,"fail");
+    
+    k = function(e){ //attach event (defined by config)
+        e._v=e._v?e._v:[]; //if no validtor, then create
+        if(!v[1](e)){ //test if fail
+          console.log("fail",e)
+          e._v.push(l); //phail! add to fail-list
+          _.addclass(e,"fail");
         }else{
           console.log("notfail")
-          e.target._v=_.arrayremove(e.target._v,l)//passes, get rid of failures for test
-          _.removeclass(e.target,"fail");
+          e._v=_.arrayremove(e._v,l)//passes, get rid of failures for test
+          _.removeclass(e,"fail");
         }
+    }
+    for(i=c.length;i--;){ //loop elements
+      _.E(c[i], v[0], function(e){
+        k(e.target)
       })
+      k(c[i]); //run now for the heck of it
     }
   }
   _.E(h, "submit", function(e){
-    if(_.L("fail",e.target).length == 0){
+    if(_.L("fail",e.target).length != 0){
       e.preventDefault();
       alert("phail");
     }
