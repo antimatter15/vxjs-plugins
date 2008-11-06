3c9615297fc1d427c4cd22ec20b5abe40d6d114a
antimatter15 <antimatter15@0e141230-aabc-11dd-8af2-493fb0bb9f7c>
2008-11-06 04:58:46 +0000

    toarray stuff vx.log

--- trunk/vx.util.js
+++ trunk/vx.util.js
@@ -2,4 +2,5 @@ _.hasclass = function(e,c){return _.I(c,e.className.split(" "))>0}
 _.addclass = function(e,c){if(!_.hasclass(e,c))e.className+=" "+c}
 _.removeclass = function(e,c){e.className=_.arrayremove(e.className.split(" "),c).join(" ")}
 _.arrayremove = function(e,o){return (','+e.join(",")+',').split(','+o+',').join(',').split(',')}
+_.toarray = function(a){for(var b=a.length,c=[];b--;)c.push(a[b]);return c}
 //for(f=e.length;f--;)e[f]==o?f.splice(f,1):0;return e}
